66740b3f80fe4d21c9ee0b3de38c8a3dd37d68d3
Benjamin Davey <bdavey3@myune.edu.au>
2022-08-06 16:31:18 +1000

    Split application into pages and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import Home from './pages/Home';
+import Header from './components/Header';
+
 import './App.css';
 
 function App() {
-  return <></>
+  return (
+    <>
+      <Header />
+      <main className='main'>
+        <Home />
+      </main>
+    </>
+  )
 }
 
 export default App;
