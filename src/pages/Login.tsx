cc88b45ce9b4549e2add73ae12946e09f9bdd9c1
Benjamin Davey <bdavey3@myune.edu.au>
2022-08-07 21:10:51 +1000

    Add styles

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,11 @@
+import { useState } from 'react'
 import { Button, Input} from '../components'
+
+import style from './Login.module.css'
 
 export default function Login() {
   return (
-      <form>
+      <form className={style.container}>
         <Input name="username" placeholder="Username" value="" />
         <Input name="password" type="password" placeholder="Password" value="" />
         <Button type="submit">Login</Button>
