f2848d7f278812c5f2e3f99e4958d7314c444b21
Benjamin Davey <bdavey3@myune.edu.au>
2022-08-07 21:11:05 +1000

    Add styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom"
 import style from "./Header.module.css"
 
 export default function Header() {
   return (
     <header className={style.header}>
-      <div className={style.heading}>Gomoku React</div>
-      <div className={style.button}>Login</div>
+      <div className={style.container}>
+        <div>Gomoku React</div>
+        <div>Login</div>
+      </div>
     </header>
 
   )
