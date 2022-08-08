51be4a59cc57f34c835a1bbc355802b552d6f033
Benjamin Davey <bdavey3@myune.edu.au>
2022-08-08 13:16:12 +1000

    Use React Router routing in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,16 @@
+import { Link, useNavigate } from 'react-router-dom'
 import style from "./Header.module.css"
 
 export default function Header() {
+  const navigate = useNavigate()
+  
   return (
     <header className={style.header}>
       <div className={style.container}>
-        <div>Gomoku React</div>
-        <div>Login</div>
+        <Link to="/">Gomoku React</Link>
+        <div className={style.actions}>
+          <button className={style.action} onClick={() => navigate('login')}>Login</button>
+        </div>
       </div>
     </header>
 
