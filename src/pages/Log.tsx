d2b322671c6f6b70dfb03915d6d4725f1715e695
Benjamin Davey <bdavey3@myune.edu.au>
2022-08-08 22:05:36 +1000

    Protect log from unauthorised users

--- src/pages/Log.tsx
+++ src/pages/Log.tsx
@@ -1,4 +1,11 @@
+import { useContext } from "react"
+import { Navigate } from "react-router-dom"
+import { UserContext } from "../context"
+
 export default function Log() {
+  const { user } = useContext(UserContext)
+  if (!user) return <Navigate to='/login' />
+  
   return (
     <div>Log</div>
   )
