366523585e20f5ad3c396802aa5f65c69d8cbaff
Benjamin Davey <bdavey3@myune.edu.au>
2022-08-09 22:01:56 +1000

    Add enums

---
+++ src/constants/index.ts
@@ -0,0 +1,13 @@
+export enum GAME_STATUS {
+    BLACK = "BLACK",
+    WHITE = "WHITE",
+    DRAW = "DRAW",
+    BLACKWIN = "BLACKWIN",
+    WHITEWIN = "WHITEWIN",
+}
+
+export enum SQUARE_STATUS {
+    EMPTY = "EMPTY",
+    BLACK = "BLACK",
+    WHITE = "WHITE",
+}
