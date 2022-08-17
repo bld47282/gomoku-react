4b171afb6dd6a23f71f835bd5168563ea5127c90
Benjamin Davey <bdavey3@myune.edu.au>
2022-08-17 16:02:52 +1000

    Change game size on Start button click

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,22 @@
 import { Button, NumberDropdown } from '../components'
 import { useNavigate } from 'react-router-dom'
+import { useContext } from 'react'
 
 
 import style from './Home.module.css'
+import { UserContext } from '../context'
 
 export default function Home() {
   const navigate = useNavigate()
+  const user = useContext(UserContext)
 
   return (
     <div className={style.container}>
       <NumberDropdown start={5} end={19}></NumberDropdown>
-      <Button onClick={() => navigate("/game")}>Start</Button>
+      <Button onClick={() => {
+        navigate("/game")
+        user.gameSize = 19
+      }}>Start</Button>
     </div>
   )
 }
