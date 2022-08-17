73f3dc600be0a46d9d7af70714d795998e3566fd
Benjamin Davey <bdavey3@myune.edu.au>
2022-08-17 15:18:03 +1000

    Add Squares to test History page

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,12 +1,20 @@
 import { useContext } from "react"
 import { Navigate } from "react-router-dom"
 import { UserContext } from "../context"
+import { Square } from "../components"
+import { SQUARE_STATUS } from "../constants"
 
 export default function History() {
   const { user } = useContext(UserContext)
   if (!user) return <Navigate to='/login' />
   
   return (
-    <div>History</div>
+    <>
+      <div>History</div>
+      <Square state={SQUARE_STATUS.BLACK} turn={4}></Square>
+      <Square state={SQUARE_STATUS.WHITE} turn={4}></Square>
+      <Square state={SQUARE_STATUS.BLACK} turn={4}></Square>
+      <Square state={SQUARE_STATUS.WHITE} turn={4}></Square>
+    </>
   )
 }
