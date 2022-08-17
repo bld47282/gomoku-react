40cef40536114389046364d936984152e9af2eec
Benjamin Davey <bdavey3@myune.edu.au>
2022-08-17 15:16:19 +1000

    Navigate to Game page when Start is pressed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,16 @@
 import { Button, NumberDropdown } from '../components'
+import { useNavigate } from 'react-router-dom'
+
 
 import style from './Home.module.css'
 
 export default function Home() {
+  const navigate = useNavigate()
+
   return (
     <div className={style.container}>
       <NumberDropdown start={5} end={19}></NumberDropdown>
-      <Button>Start</Button>
+      <Button onClick={() => navigate("/game")}>Start</Button>
     </div>
   )
 }
