81929ec033d5e27d058b2d2b8a2ed77122351a46
Benjamin Davey <bdavey3@myune.edu.au>
2022-08-19 20:20:58 +1000

    Add classNames to Square components

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -2,8 +2,21 @@ import style from "./Square.module.css"
 import { SQUARE_STATUS } from "../constants/index"
 import { Square as SquareProps } from '../types'
 
+const getClassNames = (status: SQUARE_STATUS) => {
+  switch(status) {
+    case SQUARE_STATUS.EMPTY:
+      return `${style.square} ${style.empty}`
+    case SQUARE_STATUS.BLACK:
+      return `${style.square} ${style.black}`
+    case SQUARE_STATUS.WHITE:
+      return `${style.square} ${style.white}` 
+    default:
+      return `${style.square}` 
+  }
+}
+
 export default function Square(props: SquareProps) {
   return (
-    <div className={`${style.square} ${style.black}`}></div>
+    <div className={getClassNames(props.state)}>{props.turn}</div>
   )
 }
