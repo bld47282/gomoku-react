b7180c37c5179599abbbe7c1bf0895c1a031b3cf
Benjamin Davey <bdavey3@myune.edu.au>
2022-08-19 20:20:05 +1000

    Remove test squares from History page

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,8 +1,6 @@
 import { useContext } from "react"
 import { Navigate } from "react-router-dom"
 import { UserContext } from "../context"
-import { Square } from "../components"
-import { SQUARE_STATUS } from "../constants"
 
 export default function History() {
   const { user } = useContext(UserContext)
@@ -11,10 +9,6 @@ export default function History() {
   return (
     <>
       <div>History</div>
-      <Square state={SQUARE_STATUS.BLACK} turn={4}></Square>
-      <Square state={SQUARE_STATUS.WHITE} turn={4}></Square>
-      <Square state={SQUARE_STATUS.BLACK} turn={4}></Square>
-      <Square state={SQUARE_STATUS.WHITE} turn={4}></Square>
     </>
   )
 }
