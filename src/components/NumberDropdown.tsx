b2c83b70bb4765363dfe57873e071614d225532d
Benjamin Davey <bdavey3@myune.edu.au>
2022-08-19 19:04:16 +1000

    Add onChange prop to NumberDropdown component

--- src/components/NumberDropdown.tsx
+++ src/components/NumberDropdown.tsx
@@ -1,18 +1,19 @@
 type NumberDropdownProps = {
   start: number
   end: number
+  onChange: (e: any) => void
 }
 
-export default function NumberDropdown({start, end} : NumberDropdownProps) {
+export default function NumberDropdown({start, end, onChange} : NumberDropdownProps) {
   let numbers: number[] = []
   for (let i = start; i <= end; i++) {
     numbers.push(i)
   }
 
   return (
-    <select>
-      <option hidden>Board size:</option>
-      {numbers.map((x) => <option>{x}</option>)}
+    <select onChange={onChange}>
+      <option value={19} hidden>Board size:</option>
+      {numbers.map((x) => <option value={x}>{x}</option>)}
     </select>
   )
 }
