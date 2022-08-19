d82bd45ca6a3125fb0ac78ab2eea6ad4f3dbe2f6
Benjamin Davey <bdavey3@myune.edu.au>
2022-08-19 19:07:00 +1000

    Display board on Game page

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,5 +1,41 @@
+import { useContext, useState } from "react"
+import style from './Game.module.css'
+import { SQUARE_STATUS } from "../constants"
+import { UserContext } from "../context"
+import { Square as SquareType } from "../types"
+import { Square } from "../components"
+
 export default function Game() {  
+  const user = useContext(UserContext)
+  const [board, setBoard] = useState<SquareType[] | null>(null)
+
+  if (!user.gameSize) {
+    user.gameSize = 19
+  }
+
+  if (!board) {
+    const newBoard: SquareType[] = []
+    for (let i = 0; i < (user.gameSize * user.gameSize); i++) {
+      const newSquare: SquareType = {
+        state: SQUARE_STATUS.BLACK, 
+        turn: null
+      }
+      newBoard.push(newSquare) 
+    }
+    setBoard(newBoard)
+  }
+
+  console.log(board)
   return (
-    <div>Game</div>
+    <div className={style.container}>
+      <div 
+        className={style.grid}
+        style={{ gridTemplateColumns: `repeat(${user.gameSize}, 1fr)`}}
+      >
+        {board?.map((x, index) => (
+          <Square state={x.state} turn={x.turn} key={index}></Square>
+        ))}
+      </div>
+    </div>
   )
 }
