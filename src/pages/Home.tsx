cd9b8afead85ba095a80ea394b64ff5299bf8c97
Benjamin Davey <bdavey3@myune.edu.au>
2022-08-20 12:55:06 +1000

    Store gameSize in State instead of Context

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,22 @@
 import { Button, NumberDropdown } from '../components'
-import { useNavigate } from 'react-router-dom'
-import { useContext } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useState } from 'react'
 
 
 import style from './Home.module.css'
-import { UserContext } from '../context'
 
 export default function Home() {
   const navigate = useNavigate()
-  const user = useContext(UserContext)
+  const [gameSize, setGameSize] = useState<number>(19)
   const handleChange = (e: any) => {
-    user.gameSize = e.target.value
+    setGameSize(e.target.value)
   }
 
   return (
     <div className={style.container}>
       <NumberDropdown start={5} end={19} onChange={handleChange}></NumberDropdown>
       <Button onClick={() => {
-        navigate("/game")
+        navigate("/game"+gameSize)
       }}>Start</Button>
     </div>
   )
