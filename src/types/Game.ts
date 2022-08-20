708b651c4d1c73f536cf65ca27c1f9be83500270
Benjamin Davey <bdavey3@myune.edu.au>
2022-08-20 15:37:50 +1000

    Add board as parameter to Game type

--- src/types/Game.ts
+++ src/types/Game.ts
@@ -1,7 +1,9 @@
 import { GAME_STATUS } from "../constants"
+import { Square } from "./Square"
 
 export type Game = {
     gameSize: number
     gameState: GAME_STATUS
     turn: number
+    board: Square[]
 }
