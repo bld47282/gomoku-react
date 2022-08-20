8b40c78c2b0b718323a9e30a0b938eb942e5029e
bld47282 <benjamin.leigh.davey@gmail.com>
2022-08-21 09:48:32 +1000

    Move game logic to new file

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -6,29 +6,45 @@ import { UserContext } from "../context"
 import { Game as GameType, Square as SquareType } from "../types"
 import { Square } from "../components"
 import { Navigate } from "react-router-dom"
+import { hasWon, isDraw } from "../utils"
 
 export default function Game() {
   const { user } = useContext(UserContext)
   const { size } = useParams()
-  const parsedSize = size? parseInt(size) : 19
+  const parsedSize = size ? parseInt(size) : 19
 
   const updateBoard = (id: number) => {
+    if (game.gameState !== GAME_STATUS.WHITE && game.gameState !== GAME_STATUS.BLACK) return
     const updatedSquare: SquareType = {
       id: id,
       state: (game.gameState === GAME_STATUS.BLACK) ? SQUARE_STATUS.BLACK : SQUARE_STATUS.WHITE,
       turn: game.turn
     }
 
-    setGame( {
+    setGame({
       gameSize: game.gameSize,
       gameState: (game.gameState === GAME_STATUS.BLACK) ? GAME_STATUS.WHITE : GAME_STATUS.BLACK,
       turn: game.turn + 1,
       board: game.board.map((x) => ((x.id === id) ? updatedSquare : x))
-    } )
+    })
+
+    if (hasWon(updatedSquare, game) === true) setGame({
+      gameSize: game.gameSize,
+      gameState: (updatedSquare.state === SQUARE_STATUS.BLACK) ? GAME_STATUS.BLACKWIN : GAME_STATUS.WHITEWIN,
+      turn: game.turn,
+      board: game.board
+    })
+
+    if (isDraw(updatedSquare, game)) setGame({
+      gameSize: game.gameSize,
+      gameState: GAME_STATUS.DRAW,
+      turn: game.turn,
+      board: game.board
+    })
   }
 
   const newBoard: SquareType[] = []
-  
+
   for (let i = 0; i < (parsedSize * parsedSize); i++) {
     const newSquare: SquareType = {
       id: i,
@@ -38,12 +54,12 @@ export default function Game() {
     newBoard.push(newSquare)
   }
 
-  const [game, setGame] = useState<GameType>( {
-    gameSize: parsedSize, 
-    gameState: GAME_STATUS.BLACK, 
-    turn: 1, 
+  const [game, setGame] = useState<GameType>({
+    gameSize: parsedSize,
+    gameState: GAME_STATUS.BLACK,
+    turn: 1,
     board: newBoard
-  } )
+  })
 
   if (!user) return <Navigate to='/login' />
 
@@ -57,6 +73,7 @@ export default function Game() {
           <Square state={x.state} turn={x.turn} key={x.id} id={x.id} onClick={updateBoard}></Square>
         ))}
       </div>
+      <p>{game.gameState}</p>
     </div>
   )
 }
---
+++ src/utils/Rules.ts
@@ -0,0 +1,92 @@
+import { Game, Square } from "../types"
+import { GAME_STATUS } from "../constants"
+
+export function hasWon(lastMove: Square, game: Game): boolean {
+    var victory: boolean = false
+
+    // Condition one - up/down
+    var c1: number = 1
+    for (let i = 1; i < game.gameSize; i++) {
+        if (game.board[lastMove.id + (game.gameSize * i)]?.state === lastMove.state) {
+            c1++
+        } else {
+            break
+        }
+    }
+
+    for (let i = 1; i < game.gameSize; i++) {
+        if (game.board[lastMove.id - (game.gameSize * i)]?.state === lastMove.state) {
+            c1++
+        } else {
+            break
+        }
+    }
+
+    if (c1 === 5) victory = true
+
+    // Condition two - left-right
+    var c2: number = 1
+    for (let i = 1; i < game.gameSize; i++) {
+        if (game.board[lastMove.id + i]?.state === lastMove.state) {
+            c2++
+        } else {
+            break
+        }
+    }
+
+    for (let i = 1; i < game.gameSize; i++) {
+        if (game.board[lastMove.id - i]?.state === lastMove.state) {
+            c2++
+        } else {
+            break
+        }
+    }
+
+    if (c2 === 5) victory = true
+
+    // Condition three - tr-bl
+    var c3: number = 1
+    for (let i = 1; i < game.gameSize; i++) {
+        if (game.board[lastMove.id + (game.gameSize * i) + i]?.state === lastMove.state) {
+            c3++
+        } else {
+            break
+        }
+    }
+
+    for (let i = 1; i < game.gameSize; i++) {
+        if (game.board[lastMove.id - (game.gameSize * i) - i]?.state === lastMove.state) {
+            c3++
+        } else {
+            break
+        }
+    }
+
+    if (c3 === 5) victory = true
+
+    // Condition four bl-tr
+    var c4: number = 1
+    for (let i = 1; i < game.gameSize; i++) {
+        if (game.board[lastMove.id + (game.gameSize * i) + i]?.state === lastMove.state) {
+            c4++
+        } else {
+            break
+        }
+    }
+
+    for (let i = 1; i < game.gameSize; i++) {
+        if (game.board[lastMove.id - (game.gameSize * i) - i]?.state === lastMove.state) {
+            c4++
+        } else {
+            break
+        }
+    }
+
+    if (c4 === 5) victory = true
+
+    return victory
+}
+
+export function isDraw(lastMove: Square, game: Game): boolean {
+    return (game.turn === (game.gameSize * game.gameSize)) && (game.gameState !== GAME_STATUS.BLACK) && (game.gameState !== GAME_STATUS.WHITE) ? true : false
+}
