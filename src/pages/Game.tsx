315deafd04ea41fda8f70d29a0aa45cb64d54f37
Benjamin Davey <bdavey3@myune.edu.au>
2022-08-20 12:56:29 +1000

    Use Game object for state instead of UserContext

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,39 +1,41 @@
 import { useContext, useState } from "react"
+import { useParams } from "react-router-dom"
 import style from './Game.module.css'
 import { GAME_STATUS, SQUARE_STATUS } from "../constants"
 import { UserContext } from "../context"
-import { Square as SquareType } from "../types"
+import { Game as GameType, Square as SquareType } from "../types"
 import { Square } from "../components"
 import { Navigate } from "react-router-dom"
 
 export default function Game() {
-  const user = useContext(UserContext)
+  const { user } = useContext(UserContext)
+  const { size } = useParams()
+  const parsedSize = size? parseInt(size) : 19
+  const [game, setGame] = useState<GameType>( {gameSize: parsedSize, gameState: GAME_STATUS.BLACK, turn: 1} )
   const [board, setBoard] = useState<SquareType[] | null>(null)
 
-  if (!user.user) return <Navigate to='/login' />
-  if (!user.gameSize) user.gameSize = 19
-  if (!user.turn) user.turn = 1
-  if (!user.gameState) user.gameState = GAME_STATUS.BLACK
+  if (!user) return <Navigate to='/login' />
 
   const updateBoard = (id: number, state: SQUARE_STATUS) => {
     if (!board || state !== SQUARE_STATUS.EMPTY) return
-    console.log(id)
     const updatedSquare: SquareType = {
       id: id,
-      state: (user.gameState === GAME_STATUS.BLACK) ? SQUARE_STATUS.BLACK : SQUARE_STATUS.WHITE,
-      turn: (user.turn ? user.turn : 0),
+      state: (game.gameState === GAME_STATUS.BLACK) ? SQUARE_STATUS.BLACK : SQUARE_STATUS.WHITE,
+      turn: game.turn,
       onClick: updateBoard
     }
 
-    const updatedBoard = board?.map((x) => ((x.id === id) ? updatedSquare : x))
-    user.turn = (user.turn ? (user.turn + 1) : 1)
-    user.gameState = ((user.gameState === GAME_STATUS.BLACK) ? GAME_STATUS.WHITE : GAME_STATUS.BLACK)
-    setBoard(updatedBoard)
+    setGame({
+      gameSize: game.gameSize,
+      gameState: (game.gameState === GAME_STATUS.BLACK) ? GAME_STATUS.WHITE : GAME_STATUS.BLACK,
+      turn: game.turn + 1
+    })
+    setBoard(board?.map((x) => ((x.id === id) ? updatedSquare : x)))
   }
 
   if (!board) {
     const newBoard: SquareType[] = []
-    for (let i = 0; i < (user.gameSize * user.gameSize); i++) {
+    for (let i = 0; i < (game.gameSize * game.gameSize); i++) {
       const newSquare: SquareType = {
         id: i,
         state: SQUARE_STATUS.EMPTY,
@@ -49,7 +51,7 @@ export default function Game() {
     <div className={style.container}>
       <div
         className={style.grid}
-        style={{ gridTemplateColumns: `repeat(${user.gameSize}, 1fr)` }}
+        style={{ gridTemplateColumns: `repeat(${game.gameSize}, 1fr)` }}
       >
         {board?.map((x) => (
           <Square state={x.state} turn={x.turn} key={x.id} id={x.id} onClick={updateBoard}></Square>
