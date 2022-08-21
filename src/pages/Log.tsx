30a875e8f280f2361b38c181a6b114e185067c31
bld47282 <benjamin.leigh.davey@gmail.com>
2022-08-21 11:36:07 +1000

    Display saved games on Log page

--- src/pages/Log.tsx
+++ src/pages/Log.tsx
@@ -1,12 +1,28 @@
 import { useContext } from "react"
-import { Navigate } from "react-router-dom"
+import { Navigate, useParams } from "react-router-dom"
 import { UserContext } from "../context"
+import style from './Log.module.css'
+import { HistorySquare } from "../components"
+import { useLocalStorage } from "../hooks"
+import { Game, Square as SquareType } from "../types"
 
 export default function Log() {
   const { user } = useContext(UserContext)
+  const { id } = useParams()
+  const parsedId = id ? parseInt(id) : 0
+  const [savedGames, saveGame] = useLocalStorage<Game[]>("games", [])
   if (!user) return <Navigate to='/login' />
-  
+
   return (
-    <div>Log</div>
+    <div className={style.container}>
+      <div
+        className={style.grid}
+        style={{ gridTemplateColumns: `repeat(${savedGames[parsedId].gameSize}, 1fr)` }}
+      >
+        {savedGames[parsedId].board.map((x: SquareType) => (
+          <HistorySquare state={x.state} turn={x.turn} key={x.id} id={x.id}></HistorySquare>
+        ))}
+      </div>
+    </div>
   )
 }
