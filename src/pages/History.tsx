9892987943860fd33836ccec7174e4ff784af9a9
bld47282 <benjamin.leigh.davey@gmail.com>
2022-08-21 14:38:06 +1000

    Improve History page display

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { Navigate, useNavigate } from "react-router-dom"
 import { Button } from "../components"
+import { GAME_STATUS } from "../constants"
 import { UserContext } from "../context"
 import { useLocalStorage } from "../hooks"
 import { Game } from "../types"
@@ -12,17 +13,32 @@ export default function History() {
   const [savedGames, saveGame] = useLocalStorage<Game[]>("games", [])
   if (!user) return <Navigate to='/login' />
 
-  console.log(savedGames)
+  function gameStateToString(status: GAME_STATUS): string {
+    switch(status) {
+      case GAME_STATUS.BLACKWIN:
+        return "Winner: Black"
+      case GAME_STATUS.WHITE:
+        return "Winner: White"
+      case GAME_STATUS.DRAW:
+        return "Draw"
+      default:
+        return "Undetermined result"
+    }
+  }
 
   return (
     <>
       <div className={style.container}>
-        <p>History</p>
-        {savedGames.map((x, index) => (
-          <Button key={index} onClick={() => (
-            navigate("/log" + index)
-          )}>{index}</Button>
-        ))}
+        <div className={style.cardsContainer}>
+          {savedGames.map((x, index) => (
+            <div className={style.card}>
+              Game #{index}: {gameStateToString(x.gameState)}
+              <Button key={index} onClick={() => (
+                navigate("/log" + index)
+              )}>View</Button>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   )
