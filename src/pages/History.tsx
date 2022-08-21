68affe256b9e8209b3f09d337c15259a1d8ead54
bld47282 <benjamin.leigh.davey@gmail.com>
2022-08-21 11:36:27 +1000

    Display saved games on History page

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,14 +1,29 @@
 import { useContext } from "react"
-import { Navigate } from "react-router-dom"
+import { Navigate, useNavigate } from "react-router-dom"
+import { Button } from "../components"
 import { UserContext } from "../context"
+import { useLocalStorage } from "../hooks"
+import { Game } from "../types"
+import style from './History.module.css'
 
 export default function History() {
   const { user } = useContext(UserContext)
+  const navigate = useNavigate()
+  const [savedGames, saveGame] = useLocalStorage<Game[]>("games", [])
   if (!user) return <Navigate to='/login' />
+
+  console.log(savedGames)
 
   return (
     <>
-      <div>History</div>
+      <div className={style.container}>
+        <p>History</p>
+        {savedGames.map((x, index) => (
+          <Button key={index} onClick={() => (
+            navigate("/log" + index)
+          )}>{index}</Button>
+        ))}
+      </div>
     </>
   )
 }
