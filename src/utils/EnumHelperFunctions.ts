786bfb63f30e6c89986633fd720927b427195001
bld47282 <benjamin.leigh.davey@gmail.com>
2022-08-21 14:42:09 +1000

    Generalise gameStateToString

--- src/utils/EnumHelperFunctions.ts
+++ src/utils/EnumHelperFunctions.ts
@@ -4,11 +4,15 @@ export function gameStateToString(status: GAME_STATUS): string {
     switch (status) {
         case GAME_STATUS.BLACKWIN:
             return "Winner: Black"
-        case GAME_STATUS.WHITE:
+        case GAME_STATUS.WHITEWIN:
             return "Winner: White"
         case GAME_STATUS.DRAW:
             return "Draw"
+        case GAME_STATUS.BLACK:
+            return "Black's turn"
+        case GAME_STATUS.WHITE:
+            return "White's turn"
         default:
-            return "Undetermined result"
+            return "Undermined"
     }
 }
