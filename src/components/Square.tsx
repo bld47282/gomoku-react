26df5dacec69129c2792328ffae144c673e984eb
bld47282 <benjamin.leigh.davey@gmail.com>
2022-08-21 16:25:44 +1000

    Improve Square styling

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -9,23 +9,25 @@ type SquareProps = {
 }
 
 const getClassNames = (status: SQUARE_STATUS) => {
-  switch(status) {
+  switch (status) {
     case SQUARE_STATUS.EMPTY:
       return `${style.square} ${style.empty}`
     case SQUARE_STATUS.BLACK:
       return `${style.square} ${style.black}`
     case SQUARE_STATUS.WHITE:
-      return `${style.square} ${style.white}` 
+      return `${style.square} ${style.white}`
     default:
-      return `${style.square}` 
+      return `${style.square}`
   }
 }
 
 export default function Square(props: SquareProps) {
   return (
-    <div 
-      className={getClassNames(props.state)}
-      onClick={(e) => (props.state === SQUARE_STATUS.EMPTY) ? (props.onClick(props.id)) : e.preventDefault()}>
+    <div className={style.boardSquare}>
+      <div
+        className={getClassNames(props.state)}
+        onClick={(e) => (props.state === SQUARE_STATUS.EMPTY) ? (props.onClick(props.id)) : e.preventDefault()}>
+      </div>
     </div>
   )
 }
