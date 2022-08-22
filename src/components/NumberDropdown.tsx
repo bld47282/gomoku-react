cafdc690b2d2cabff6c9af2d952f30a3191bb33b
bld47282 <benjamin.leigh.davey@gmail.com>
2022-08-22 14:57:11 +1000

    Improve NumberDropdown styling

--- src/components/NumberDropdown.tsx
+++ src/components/NumberDropdown.tsx
@@ -1,19 +1,31 @@
-type NumberDropdownProps = {
-  start: number
-  end: number
-  onChange: (e: any) => void
-}
+import style from "./NumberDropdown.module.css"
 
-export default function NumberDropdown({start, end, onChange} : NumberDropdownProps) {
-  let numbers: number[] = []
+type NumberDropdownProps = {
+  start: number;
+  end: number;
+  onChange: (e: any) => void;
+};
+
+export default function NumberDropdown({
+  start,
+  end,
+  onChange,
+}: NumberDropdownProps) {
+  let numbers: number[] = [];
   for (let i = start; i <= end; i++) {
-    numbers.push(i)
+    numbers.push(i);
   }
 
   return (
-    <select onChange={onChange}>
-      <option value={19} key={0} hidden>Board size:</option>
-      {numbers.map((x) => <option key ={x} value={x}>{x}</option>)}
+    <select className={style.select} onChange={onChange}>
+      <option value={19} key={0} hidden>
+        Board size:
+      </option>
+      {numbers.map((x) => (
+        <option key={x} value={x}>
+          {x}
+        </option>
+      ))}
     </select>
-  )
+  );
 }
