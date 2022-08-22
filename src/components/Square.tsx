d6c90cadf53025f2891f71dc1a8bae8a221476f5
bld47282 <benjamin.leigh.davey@gmail.com>
2022-08-22 22:09:13 +1000

    Format with Prettier

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,33 +1,38 @@
-import style from "./Square.module.css"
-import { SQUARE_STATUS } from "../constants/index"
+import style from "./Square.module.css";
+import { SQUARE_STATUS } from "../constants/index";
 
 type SquareProps = {
-  id: number
-  state: SQUARE_STATUS
-  turn: number | null
-  onClick: (id: number) => void
-}
+  id: number;
+  state: SQUARE_STATUS;
+  turn: number | null;
+  onClick: (id: number) => void;
+};
 
 const getClassNames = (status: SQUARE_STATUS) => {
   switch (status) {
     case SQUARE_STATUS.EMPTY:
-      return `${style.square} ${style.empty}`
+      return `${style.square} ${style.empty}`;
     case SQUARE_STATUS.BLACK:
-      return `${style.square} ${style.black}`
+      return `${style.square} ${style.black}`;
     case SQUARE_STATUS.WHITE:
-      return `${style.square} ${style.white}`
+      return `${style.square} ${style.white}`;
     default:
-      return `${style.square}`
+      return `${style.square}`;
   }
-}
+};
 
 export default function Square(props: SquareProps) {
   return (
     <div className={style.boardSquare}>
       <div
         className={getClassNames(props.state)}
-        onClick={(e) => (props.state === SQUARE_STATUS.EMPTY) ? (props.onClick(props.id)) : e.preventDefault()}>
-      </div>
+        onClick={(e) =>
+          props.state === SQUARE_STATUS.EMPTY
+            ? props.onClick(props.id)
+            : e.preventDefault()
+        }
+        data-testid="square"
+      ></div>
     </div>
-  )
+  );
 }
